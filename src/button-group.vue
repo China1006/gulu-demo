7f5c1e7e8c9a8710bad37fdaa7966d39604ab2a2
邓国新 <1414894071@qq.com>
2020-08-19 11:49:47 +0800

    检测子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script lang='ts'>
+  export default {
+    mounted(){
+      for (let node of this.$el.children){
+        let name = node.nodeName.toLowerCase()
+        if ( name !== 'button'){
+          console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
+        }
+      }
+    }
+  }
 </script>
 
 <style lang='scss' >
@@ -13,7 +23,7 @@
     vertical-align: middle;
     > .g-button{
       border-radius: 0;
-      margin-left: -1px;
+      margin-left: -1px;//右移
 
       &:first-child {
         border-top-left-radius: var(--border-radius);
@@ -24,7 +34,7 @@
         border-top-right-radius: var(--border-radius);
         border-bottom-right-radius: var(--border-radius);
       }
-      &:hover{
+      &:hover{//浮起来
         position: relative;
         z-index: 1;
       }
