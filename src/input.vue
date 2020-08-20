497f94df112288693627b898036a685ed100e891
邓国新 <1414894071@qq.com>
2020-08-20 14:30:21 +0800

    添加input.vue及其样式

---
+++ src/input.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :value="value"
+           :disabled="disabled"
+           :readonly="readonly"
+           type="text">
+    <template v-if="error">
+      <Icon name="error" class="icon-error"></Icon>
+      <span class="error-message">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script lang='ts'>
+  import Icon from './icon'
+  export default {
+    name: 'GuluInput',
+    components: {Icon},
+    props:{
+      value:{
+        type: String
+      },
+      disabled:{
+        type: Boolean,
+        default: false
+      },
+      readonly:{
+        type: Boolean,
+        default: false
+      },
+      error:{
+        type: String
+      }
+    }
+  };
+</script>
+
+<style lang='scss' scoped>
+  $height: 32px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 12px;
+  $box-shadow-color: rgba(0,0,0,0.5);
+  $red: #f1453d;
+  .wrapper {font-size: $font-size;display: inline-flex;align-items: center;
+    >:not(:last-child){margin-right: .5em;}
+    > input {height: $height;border: 1px solid  $border-color;border-radius: $border-radius;padding: 0 8px;font-size: inherit;
+      &:hover{border-color: $border-color-hover;}
+      &:focus{box-shadow: inset 0 1px 3px $box-shadow-color;outline: none}
+      &[disabled],&[readonly]{border-color: #bbb;color: #bbb;cursor: no-drop;}
+    }
+    &.error{
+      >input{border-color: $red;} }
+    .icon-error{fill: $red;}
+    .error-message{color: $red;}
+  }
+</style>
