33aa34edf025c5c617c9f87a23ccd835c0efa1d8
邓国新 <1414894071@qq.com>
2020-08-20 19:42:06 +0800

    row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    
+  </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'GuluCol',
+    props: {
+      span: {
+        type: [Number,String]
+      }
+    }
+  };
+</script>
+
+<style lang='scss' scoped>
+  .col{
+    height: 100px;
+    background-color: gray;
+    width: 50%;
+    border: 1px solid red;
+    $class: col-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n}{
+      width: ($n / 24) * 100%;
+    }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'row'
+  };
+</script>
+
+<style lang='scss' scoped>
+  .row{
+    display: flex;
+  }
+</style>
