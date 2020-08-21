412e5e576773ef9c31c77c4d7871426f982e4915
邓国新 <1414894071@qq.com>
2020-08-21 16:52:05 +0800

    创建layout content footer sider header组件

---
+++ src/layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'Gululayout'
+  };
+</script>
+
+<style lang='scss' scoped>
+  .layout{
+
+  }
+</style>
