447d7e232638eb3996528ae23fe09daaac96bd58
邓国新 <1414894071@qq.com>
2020-08-24 19:28:41 +0800

    触发update:selected事件的时候添加一个item数据

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line"></div>
     <div class="action-wrapper">
       <slot name="actions" ></slot>
     </div>
@@ -12,17 +13,28 @@
     name: 'GuluTabsHead',
     inject: ['eventBus'],
     created(){
+      this.eventBus.$on('update:selected',(item)=>{
+
+      })
     }
   };
 </script>
 
 <style lang='scss' scoped>
   $tab-height: 40px;
+  $blue: blue;
 .tabs-head{
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
   border: 1px solid red;
+  position: relative;
+  > .line{
+    position: absolute;
+    bottom: 0;
+    border-bottom: 1px solid $blue;
+    width: 100px;
+  }
   > .action-wrapper{
     margin-left: auto;
   }
--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -38,13 +38,14 @@
     },
     methods:{
       xxx(){
-        this.eventBus.$emit('update:selected',this.name)
+        this.eventBus.$emit('update:selected',this.name,this)
       }
     }
   };
 </script>
 
 <style lang='scss' scoped>
+  $blue: blue;
 .tabs-item{
   flex-shrink: 0;
   padding: 0 1em;
@@ -53,7 +54,8 @@
   display: flex;
   align-items: center;
   &.active{
-    background: blue;
+    color: $blue;
+    font-weight: bold;
   }
 }
 </style>
